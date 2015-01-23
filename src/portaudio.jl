7fdd37b73f53f2097caff8c6f014b5e1dca7c94d
flpf <fpfeifle@gmail.com>
2015-01-23 20:37:42 +0100

    Update portaudio.jl

--- src/portaudio.jl
+++ src/portaudio.jl
@@ -22,7 +22,7 @@ const paInt8    = convert(PaSampleFormat, 0x10)
 const paUInt8   = convert(PaSampleFormat, 0x20)
 
 # PaHostApiTypeId values
-@compat const pa_host_api_names = {
+ const pa_host_api_names =@compat {
     0 => "In Development", # use while developing support for a new host API
     1 => "Direct Sound",
     2 => "MME",
